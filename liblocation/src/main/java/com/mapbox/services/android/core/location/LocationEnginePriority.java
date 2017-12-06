6775a3cbaaeb73a758d92c748722583bc5e1d485
Anthony Agby <agbyaj@gmail.com>
2017-12-06 11:55:34 -0500

    Make Priority Editable (#18)
    - make priority editable

--- liblocation/src/main/java/com/mapbox/services/android/core/location/LocationEnginePriority.java
+++ liblocation/src/main/java/com/mapbox/services/android/core/location/LocationEnginePriority.java
@@ -1,5 +1,10 @@
 package com.mapbox.services.android.core.location;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Same priorities GMS and Lost support:
  * <p>
@@ -11,4 +16,9 @@ public class LocationEnginePriority {
   public static final int LOW_POWER = 1;
   public static final int BALANCED_POWER_ACCURACY = 2;
   public static final int HIGH_ACCURACY = 3;
+
+  @IntDef({NO_POWER, LOW_POWER, BALANCED_POWER_ACCURACY, HIGH_ACCURACY})
+  @Retention(RetentionPolicy.SOURCE)
+  public @interface PowerMode {
+  }
 }
