856f74fac89370248ac54dbaa7b6dc366ab5f67b
electrostat <agbyaj@gmail.com>
2018-03-22 19:43:32 -0400

    Default value
    - change out if - parameters
    - use default value for metadata tag

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/TelemetryEnabler.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/TelemetryEnabler.java
@@ -78,8 +78,8 @@ public class TelemetryEnabler {
       ApplicationInfo appInformation = context.getPackageManager().getApplicationInfo(
         context.getPackageName(), PackageManager.GET_META_DATA);
 
-      if (appInformation != null && appInformation.metaData.containsKey(KEY_META_DATA_ENABLED)) {
-        boolean isEnabled = appInformation.metaData.getBoolean(KEY_META_DATA_ENABLED);
+      if (appInformation != null && appInformation.metaData != null) {
+        boolean isEnabled = appInformation.metaData.getBoolean(KEY_META_DATA_ENABLED, true);
         return isEnabled;
       }
     } catch (PackageManager.NameNotFoundException exception) {
