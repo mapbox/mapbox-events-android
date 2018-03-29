b42dce31eda665cb7bfad604d90da3e2058a6134
Anthony Agby <agbyaj@gmail.com>
2018-03-29 14:49:40 -0400

    TelemetryUtils changes
    - getVolumeLevel
    - getScreenBrightness
    - getApplicationState
    - get CellularNetwork

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/TelemetryUtils.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/TelemetryUtils.java
@@ -1,12 +1,17 @@
 package com.mapbox.android.telemetry;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
+import android.media.AudioManager;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -21,6 +26,105 @@ public class TelemetryUtils {
   private static final String THREE_STRING_FORMAT = "%s/%s/%s";
   private static final SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_AND_TIME_PATTERN, Locale.US);
   private static final Locale DEFAULT_LOCALE = Locale.US;
+
+
+  public static String toHumanReadableAscii(String s) {
+    for (int i = 0, length = s.length(), c; i < length; i += Character.charCount(c)) {
+      c = s.codePointAt(i);
+      if (c > '\u001f' && c < '\u007f') {
+        continue;
+      }
+
+      Buffer buffer = new Buffer();
+      buffer.writeUtf8(s, 0, i);
+      for (int j = i; j < length; j += Character.charCount(c)) {
+        c = s.codePointAt(j);
+        buffer.writeUtf8CodePoint(c > '\u001f' && c < '\u007f' ? c : '?');
+      }
+      return buffer.readUtf8();
+    }
+    return s;
+  }
+
+  public static int getVolumeLevel(Context context) {
+    AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    return (int) Math.floor(100.0 * audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+      / audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+  }
+
+  public static int getScreenBrightness(Context context) {
+    int screenBrightness;
+    try {
+      screenBrightness = android.provider.Settings.System.getInt(
+        context.getContentResolver(),
+        android.provider.Settings.System.SCREEN_BRIGHTNESS);
+
+      // Android returns values between 0 and 255, here we normalize to 0-100.
+      screenBrightness = (int) Math.floor(100.0 * screenBrightness / 255.0);
+    } catch (Settings.SettingNotFoundException exception) {
+      screenBrightness = -1;
+    }
+
+    return screenBrightness;
+  }
+
+  public static String getApplicationState(Context context) {
+    ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+    if (appProcesses == null) {
+      return "";
+    }
+
+    String packageName = context.getPackageName();
+    for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+      if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+        && appProcess.processName.equals(packageName)) {
+        return "Foreground";
+      }
+    }
+
+    return "Background";
+  }
+
+  public static String getCellularNetworkType(Context context) {
+    TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    switch (manager.getNetworkType()) {
+      case TelephonyManager.NETWORK_TYPE_1xRTT:
+        return "1xRTT";
+      case TelephonyManager.NETWORK_TYPE_CDMA:
+        return "CDMA";
+      case TelephonyManager.NETWORK_TYPE_EDGE:
+        return "EDGE";
+      case TelephonyManager.NETWORK_TYPE_EHRPD:
+        return "EHRPD";
+      case TelephonyManager.NETWORK_TYPE_EVDO_0:
+        return "EVDO_0";
+      case TelephonyManager.NETWORK_TYPE_EVDO_A:
+        return "EVDO_A";
+      case TelephonyManager.NETWORK_TYPE_EVDO_B:
+        return "EVDO_B";
+      case TelephonyManager.NETWORK_TYPE_GPRS:
+        return "GPRS";
+      case TelephonyManager.NETWORK_TYPE_HSDPA:
+        return "HSDPA";
+      case TelephonyManager.NETWORK_TYPE_HSPA:
+        return "HSPA";
+      case TelephonyManager.NETWORK_TYPE_HSPAP:
+        return "HSPAP";
+      case TelephonyManager.NETWORK_TYPE_HSUPA:
+        return "HSUPA";
+      case TelephonyManager.NETWORK_TYPE_IDEN:
+        return "IDEN";
+      case TelephonyManager.NETWORK_TYPE_LTE:
+        return "LTE";
+      case TelephonyManager.NETWORK_TYPE_UMTS:
+        return "UMTS";
+      case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+        return "Unknown";
+      default:
+        return "";
+    }
+  }
 
   static String obtainCurrentDate() {
     return dateFormat.format(new Date());
@@ -89,23 +193,5 @@ public class TelemetryUtils {
     } catch (Exception exception) {
       return EMPTY_STRING;
     }
-  }
-
-  public static String toHumanReadableAscii(String s) {
-    for (int i = 0, length = s.length(), c; i < length; i += Character.charCount(c)) {
-      c = s.codePointAt(i);
-      if (c > '\u001f' && c < '\u007f') {
-        continue;
-      }
-
-      Buffer buffer = new Buffer();
-      buffer.writeUtf8(s, 0, i);
-      for (int j = i; j < length; j += Character.charCount(c)) {
-        c = s.codePointAt(j);
-        buffer.writeUtf8CodePoint(c > '\u001f' && c < '\u007f' ? c : '?');
-      }
-      return buffer.readUtf8();
-    }
-    return s;
   }
 }
