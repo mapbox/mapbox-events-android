1bb62ca3d9db33f553134d82e8c394e933f5324c
Anthony Agby <agbyaj@gmail.com>
2018-05-04 17:20:01 -0400

    Geotrust certs
    - added new geotrust hashes

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
@@ -21,6 +21,11 @@ class ChinaCertificatePins {
           add("sha256/3coVlMAEAYhOEJHgXwloiPDGaF+ZfxHZbVoK8AYYWVg=");
           add("sha256/5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w=");
           add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
+
+          //new GeoTrust Certs
+          add("sha256/+O+QJCmvoB/FkTd0/5FvmMSvFbMqjYU+Txrw1lyGkUQ=");
+          add("sha256/zUIraRNo+4JoAYA7ROeWjARtIoN4rIEbCpfCRQT6N6A=");
+          add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
         }
       });
     }
