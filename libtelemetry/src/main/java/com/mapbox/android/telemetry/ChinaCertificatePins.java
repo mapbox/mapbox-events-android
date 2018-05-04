e2c55b1ef4c0e93b89d9a906592e2ddca87caaaf
Anthony Agby <agbyaj@gmail.com>
2018-05-04 17:03:22 -0400

    New Digicert Hashes
    - generated new digicerts hashes

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
@@ -12,7 +12,13 @@ class ChinaCertificatePins {
     {
       put("events.mapbox.cn", new ArrayList<String>() {
         {
+          //old certificates
           add("sha256/gakY+fylqW6kp6piqnaQNLZFzT8HlhzP5lsGJk5WguE=");
+          add("sha256/5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w=");
+          add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
+
+          //new digicert certificates
+          add("sha256/3coVlMAEAYhOEJHgXwloiPDGaF+ZfxHZbVoK8AYYWVg=");
           add("sha256/5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w=");
           add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
         }
