4f7cd5b8e4a7a585bef2f991de8c31ae2459daf3
Anthony Agby <agbyaj@gmail.com>
2018-05-04 17:33:43 -0400

    Add new China Certificate Hashes (#133)
    * New Digicert Hashes
    
    - generated new digicerts hashes
    
    * Geotrust certs
    
    - added new geotrust hashes

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
@@ -12,8 +12,19 @@ class ChinaCertificatePins {
     {
       put("events.mapbox.cn", new ArrayList<String>() {
         {
+          //old certificates
           add("sha256/gakY+fylqW6kp6piqnaQNLZFzT8HlhzP5lsGJk5WguE=");
           add("sha256/5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w=");
+          add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
+
+          //new digicert certificates
+          add("sha256/3coVlMAEAYhOEJHgXwloiPDGaF+ZfxHZbVoK8AYYWVg=");
+          add("sha256/5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w=");
+          add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
+
+          //new GeoTrust Certs
+          add("sha256/+O+QJCmvoB/FkTd0/5FvmMSvFbMqjYU+Txrw1lyGkUQ=");
+          add("sha256/zUIraRNo+4JoAYA7ROeWjARtIoN4rIEbCpfCRQT6N6A=");
           add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
         }
       });
