911ac17d9b010ecf39bae2abe8613b3a874c6b5f
Anthony Agby <agbyaj@gmail.com>
2018-05-07 16:29:41 -0400

    unbindServiceConnection
    - create method to check if service is running and unbind if it is

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/TelemetryUtils.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/TelemetryUtils.java
@@ -216,4 +216,15 @@ public class TelemetryUtils {
     IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
     return context.registerReceiver(null, filter);
   }
+
+  static boolean isServiceRunning(Class<?> serviceClass) {
+    ActivityManager manager = (ActivityManager) MapboxTelemetry.applicationContext
+      .getSystemService(Context.ACTIVITY_SERVICE);
+    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+      if (serviceClass.getName().equals(service.service.getClassName())) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
