f166ce7c2b31cb82a2908dae70d9986b04233fde
Anthony Agby <agbyaj@gmail.com>
2018-05-21 18:17:45 -0400

    Add china hashes
    - add the new china hashes

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/ChinaCertificatePins.java
@@ -26,6 +26,58 @@ class ChinaCertificatePins {
           add("sha256/+O+QJCmvoB/FkTd0/5FvmMSvFbMqjYU+Txrw1lyGkUQ=");
           add("sha256/zUIraRNo+4JoAYA7ROeWjARtIoN4rIEbCpfCRQT6N6A=");
           add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
+
+          //new 50 hashes
+          add("T4XyKSRwZ5icOqGmJUXiDYGa+SaXKTGQXZwhqpwNTEo=");
+          add("KlV7emqpeM6V2MtDEzSDzcIob6VwkdWHiVsNQQzTIeo=");
+          add("16TK3iq9ZB4AukmDemjUyhcPTUnsSuqd5OB5zOrheZY=");
+          add("F16cPFncMDkB4XbRfK64H1dqncNg6JOdd+w2qElR/hM=");
+          add("45PQwWtFAHQd/cVzMVuhkwOQwCF+JE4ZViA4gkzvWeQ=");
+          add("mCzfopN5vqaerktI/172w8T7qw2sfRXgUL4Z7xA2e/c=");
+          add("rFFCqIOOKu7KH1v73IHb6mzZQth7cVvVDaH+EjkNfKM=");
+          add("pZBpEiH9vLwSICbogdpgyGG3NCjVKw4oG2rEWRf03Vk=");
+          add("gPgpSOzaLjbIpDLlh302x2irQTzWfsQqIWhUsreMMzI=");
+          add("wLHqvUDDQfFymRVS2O6AF5nkuAY+KJZpI4+pohK+SUE=");
+          add("yAEZR9ydeTrMlcUp91PHdmJ3lBa86IWsKRwiM0KzS6Q=");
+          add("k3NZbP68SikfwacfWDm4s3YJDsPVWJSOF4GlCWo5RJA=");
+          add("1PRG2KOhfDE+xMS1fxft5CtQO99mzqhpl4gPz/64IxQ=");
+          add("FBibSsaWfYYIkij1x4Oc9Lt0jHl+6AhBTWAypcOphhc=");
+          add("X0K6GmWp00Pb0YATdlCPeXaZR/NxxHTv41OAEkymkbU=");
+          add("DU/+Q9Itbb4WuSfuTvOgPtxtF6eAbTH7pUFn17/o5E0=");
+          add("BYGHyEqtaJEZn+02i4jy4dGRRFNr6xckQjTL7DMZFes=");
+          add("zr1/pj8y4FUbrxIYRaHVZWvhsMPzDVW0R+ljPHrX5Sw=");
+          add("fS9IR9OWsirEnSAqParPG0BzZJ+Dk4CiHfPv1vEjrf0=");
+          add("f1B7KmHknBSXNjTC8ac/Hf7hwU2goerE53TJppr0OH0=");
+          add("OKbbVU/+cTlszrJkxKaQraFAoVyjPOqa5Uq8Ndd4AUg=");
+          add("I0xGZF5s9kGHJHz6nKN+nYJKwf8ev1MdWkGt7EI7A7g=");
+          add("anATIIIqUd4o7Asto7X7OEJ+m7YTUr0aJKHZXqL92w0=");
+          add("JXFJ+lQK4GwJpJlHSZ2ZAR5luZDwMdaa2hJyhqHc1L8=");
+          add("64k4IzkPceL/hQywCCvJLQds8FPMPwtclhFOR/taKAQ=");
+          add("c079Pt5XXCwSv+pROEF+YW5gRoyzJ248bPxVLrUYkHM=");
+          add("46ofOPUGR3SYcMB+MmXqowYKan/c18LBTV2sAk13WKc=");
+          add("4qwz7KaBHxEX+YxO8STVowTg2BxlOd98GNU5feRjdjU=");
+          add("hp54/fY89ziuBBp1zv3YaC8H9/G8/Xp97hdzRVdcqQ0=");
+          add("BliQkuPecuHEp3FN3r1HogAkmsLtZz3ZImqLSpJoJzs=");
+          add("GayCH1YATG/OS5h1bq79XRmcq/aqwoObu2OYfPN7vQc=");
+          add("fW6I4HEBwa1Pwi1dldkb+ljs4re5ZY2JbsCiCxCOCgI=");
+          add("GcqilfT04N2efVIWlzJWO04gdpwYC4sLnOx3TJIKA9E=");
+          add("+1CHLRDE6ehp61cm8+NDMvd32z0Qc4bgnZRLH0OjE94=");
+          add("4vJWNxtoMLAY35dbzKeDI+4IAFOW97WNkTWnNMtY5TA=");
+          add("1YjWX9tieIA1iGkJhm7UapH6PiwGViZBWrXA3UJUAWc=");
+          add("X+RKpA7gtptrZ9yI1C96Isw5RV8dQyx5z7I/xfCaBl8=");
+          add("hqFsdAuHVvjX3NuaUBVZao94V30SdXLAsG1O0ajgixw=");
+          add("wYl9ZFQd2LWKfjDuEQxo7S0CcrPkP9A3vb20fbHf1ZQ=");
+          add("Y3ax6OgoQkcStQZ2hrIAqMDbaEEwX6xZfMZEnVcn/4k=");
+          add("taSOM7qPorxZ64Whrl5ZiNCGlZqLrVPOIBwPr/Nkw6U=");
+          add("KB5X/PyAAiRc7W/NjUyd6xbDdibuOTWBJB2MqHHF/Ao=");
+          add("hRQ7yTW/P5l76uNNP3MXNgshlmcbDNHMtBxCbUtGAWE=");
+          add("AoclhkrtKF+qHKKq0wUS4oXLwlJtWlywtiLndnNzS2U=");
+          add("5ikvGB5KkNlwesHRqjYvkZGlxP6OLMbaCkpflTM4DNM=");
+          add("qK2GksTrZ7LXDBkNWH6FnuNGxgxPpwNSK+NgknU7H1U=");
+          add("K3qyQniCBiGmfutYDE7ryDY2YoTORgp4DOgK1laOqfo=");
+          add("B7quINbFSUen02LQ9kwtYXnsJtixTpKafzXFkcRb7RU=");
+          add("Kc7lrHTlRfLaeRaEof6mKKmBH2eYHMYkxOy3yGlzUWg=");
+          add("7s1BUHi/AW/beA2jXamNTUgbDMH4gVPR9diIhnN1o0Q=");
         }
       });
     }
