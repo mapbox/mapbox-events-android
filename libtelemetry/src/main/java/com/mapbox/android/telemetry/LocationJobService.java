011f836726efdfa8d701ce41a8e5fa6cbb05891a
Anthony Agby <agbyaj@gmail.com>
2018-06-06 17:42:35 -0400

    Enhanced Location Jobs (#150)
    * LastLocation + job refactor
    
    - increase wifi calls timer
    - remove persisted key
    - lastLocation logic
    
    * 2 minutes wifi + logic adjustment
    
    - extend wifi time to 2 minutes (20 seconds)
    - move logic around
    
    * Geofence System + burstGPS
    
    - checkdistance from lastLocation
    - burstGps logic
    
    * Location Listener optimization
    
    - optimize locationListener usage
    - toggle gps and wifi on and off
    - send events after 4 cycles o
    
    * Tweak job performance
    
    - adjust radius to 100m
    - change window to every 5 minutes
    - limit gps loop to 2x to cut down on time and increase job run frequency

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/LocationJobService.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/LocationJobService.java
@@ -32,23 +32,27 @@ public class LocationJobService extends JobService implements LocationListener, 
   private final String LOG_TAG = "JobService";
   private static final int JOB_ID = 1;
   private static final int FIVE_MIN = 60 * 1000 * 5;
-  private LocationManager locationManager;
+  private LocationManager gpsLocationManager;
+  private LocationManager wifiLocationManager;
   private JobParameters currentParams;
   private ArrayList<Location> locations;
-  private boolean gpsOn;
   private String accessToken;
   private String userAgent;
+  private Location lastLocation;
+  private int radius;
+  private int gpsCount;
 
   @RequiresApi(api = Build.VERSION_CODES.N)
-  public static void schedule(Context context, String userAgent, String accessToken) {
+  public static void schedule(Context context, String userAgent, String accessToken, Location lastLocation) {
     PersistableBundle bundle = new PersistableBundle();
     bundle.putString("userAgent", userAgent);
     bundle.putString("accessToken", accessToken);
+    bundle.putDouble("latitude", lastLocation.getLatitude());
+    bundle.putDouble("longitude", lastLocation.getLongitude());
 
     ComponentName component = new ComponentName(context, LocationJobService.class);
     JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, component)
         .setPeriodic(FIVE_MIN, 2 * FIVE_MIN)
-        .setPersisted(true)
         .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
         .setExtras(bundle);
 
@@ -58,59 +62,116 @@ public class LocationJobService extends JobService implements LocationListener, 
     }
   }
 
-  @SuppressLint("MissingPermission")
   @Override
   public boolean onStartJob(JobParameters params) {
     currentParams = params;
     locations = new ArrayList<Location>();
-    gpsOn = true;
+    radius = 100;
+    gpsCount = 0;
     userAgent = params.getExtras().getString("userAgent");
     accessToken = params.getExtras().getString("accessToken");
+    lastLocation = new Location("lastLoc");
+    lastLocation.setLatitude(params.getExtras().getDouble("latitude"));
+    lastLocation.setLongitude(params.getExtras().getDouble("longitude"));
 
-    final LocationListener locationListener = this;
     Log.d(LOG_TAG,"start job");
-    Criteria criteria = new Criteria();
-    criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
-    criteria.setPowerRequirement(Criteria.POWER_HIGH);
 
-    locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-    if (locationManager != null) {
-      locationManager.getBestProvider(criteria, true);
-      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.0f, this);
-    }
-
-    startGpsTimer(locationListener);
+    checkDistanceFromLastLocation();
 
     return true;
   }
 
   @SuppressLint("MissingPermission")
-  private void startGpsTimer(final LocationListener locationListener) {
-    new CountDownTimer(20000, 1000) {
+  private void checkDistanceFromLastLocation() {
+    Log.d(LOG_TAG,"checkDistanceFromLastLocation");
+
+    LocationListener initialListener = new LocationListener() {
+      @SuppressLint("NewApi")
+      @Override
+      public void onLocationChanged(Location location) {
+        wifiLocationManager.removeUpdates(this);
+
+        float distance = lastLocation.distanceTo(location);
+
+        if (distance >= radius) {
+          Log.d(LOG_TAG,"collect Location Data");
+          startGps(20000);
+        } else {
+          Log.d(LOG_TAG,"distance too close, reschedule job, stop current one");
+          LocationJobService.schedule(getApplicationContext(), userAgent, accessToken, lastLocation);
+          jobFinished(currentParams, false);
+        }
+      }
+
+      @Override
+      public void onStatusChanged(String provider, int status, Bundle extras) {
+
+      }
+
+      @Override
+      public void onProviderEnabled(String provider) {
+
+      }
+
+      @Override
+      public void onProviderDisabled(String provider) {
+
+      }
+    };
+
+    wifiLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    wifiLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0.0f, initialListener);
+  }
+
+  @SuppressLint("MissingPermission")
+  private void startGps(long gpsMillis) {
+    Log.d(LOG_TAG,"startGPS");
+    Criteria criteria = new Criteria();
+    criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+    criteria.setPowerRequirement(Criteria.POWER_HIGH);
+
+    gpsLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    if (gpsLocationManager != null) {
+      gpsLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.0f, this);
+    }
+
+    final LocationListener locationListener = this;
+
+    new CountDownTimer(gpsMillis, 1000) {
       public void onTick(long millisUntilFinished) {
 
       }
 
+      @RequiresApi(api = Build.VERSION_CODES.N)
       public void onFinish() {
-        locationManager.removeUpdates(locationListener);
-        startWifiListener(locationListener);
+        Log.d(LOG_TAG,"gpsTimer finished");
+        gpsLocationManager.removeUpdates(locationListener);
+        gpsCount++;
+
+        if (gpsCount == 2) {
+          sendLocation(locations);
+        } else {
+          startWifiListener();
+        }
       }
     }.start();
   }
 
   @SuppressLint("MissingPermission")
-  private void startWifiListener(final LocationListener locationListener) {
-    gpsOn = false;
-    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0.0f, locationListener);
+  private void startWifiListener() {
+    wifiLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    wifiLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0.0f, this);
 
-    new CountDownTimer(20000, 1000) {
+    final LocationListener locationListener = this;
+
+    new CountDownTimer(25000, 10000) {
       public void onTick(long millisUntilFinished) {
 
       }
 
       public void onFinish() {
-        locationManager.removeUpdates(locationListener);
-        sendLocation(locations);
+        wifiLocationManager.removeUpdates(locationListener);
+        startGps(10000);
       }
     }.start();
   }
@@ -118,18 +179,13 @@ public class LocationJobService extends JobService implements LocationListener, 
   @Override
   public boolean onStopJob(JobParameters params) {
     Log.d(LOG_TAG,"stop job");
-    jobFinished(currentParams, false);
     return false;
   }
 
   @SuppressLint("MissingPermission")
   @Override
   public void onLocationChanged(Location location) {
-    Log.d(LOG_TAG,location.getLatitude() + ", " + location.getLongitude());
-
-    if (!gpsOn && location.getAccuracy() > 50) {
-      return;
-    }
+    Log.d(LOG_TAG,"location: " + location);
 
     locations.add(location);
   }
@@ -152,12 +208,14 @@ public class LocationJobService extends JobService implements LocationListener, 
   @Override
   public void onFailure(Call call, IOException e) {
     Log.d(LOG_TAG,"call failed");
-    jobFinished(currentParams, false);
+    jobFinished(currentParams, true);
   }
 
+  @RequiresApi(api = Build.VERSION_CODES.N)
   @Override
   public void onResponse(Call call, Response response) throws IOException {
     Log.d(LOG_TAG,"job finished");
+    LocationJobService.schedule(getApplicationContext(), userAgent, accessToken, lastLocation);
     jobFinished(currentParams, false);
   }
 
@@ -203,8 +261,8 @@ public class LocationJobService extends JobService implements LocationListener, 
     return secondMod + min;
   }
 
+  @RequiresApi(api = Build.VERSION_CODES.N)
   private void sendLocation(List<Location> locations) {
-    gpsOn = false;
 
     final List<Event> events = new ArrayList<>(locations.size());
 
@@ -213,15 +271,19 @@ public class LocationJobService extends JobService implements LocationListener, 
       events.add(locationEvent);
     }
 
-    final Callback callback = this;
+    if (events.size() > 0) {
+      lastLocation = locations.get(locations.size() - 1);
+      final Callback callback = this;
 
-    new Thread(new Runnable() {
-      public void run() {
-        TelemetryClient telemetryClient = createTelemetryClient();
-        telemetryClient.sendEvents(events, callback);
-      }
-    }).start();
-
-
+      new Thread(new Runnable() {
+        public void run() {
+          TelemetryClient telemetryClient = createTelemetryClient();
+          telemetryClient.sendEvents(events, callback);
+        }
+      }).start();
+    } else {
+      LocationJobService.schedule(getApplicationContext(), userAgent, accessToken, lastLocation);
+      jobFinished(currentParams, false);
+    }
   }
 }
