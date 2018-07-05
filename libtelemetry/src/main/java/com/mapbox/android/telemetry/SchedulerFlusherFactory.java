aff737bfb24a57b2fbdd807f743b499ceeed3041
Anthony Agby <agbyaj@gmail.com>
2018-07-05 16:54:24 -0400

    Adjust wakeup time
    - adjust wakeup time to 10 minutes when boolean present

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/SchedulerFlusherFactory.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/SchedulerFlusherFactory.java
@@ -3,16 +3,21 @@ package com.mapbox.android.telemetry;
 
 import android.app.AlarmManager;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 class SchedulerFlusherFactory {
   static final String SCHEDULER_FLUSHER_INTENT = "com.mapbox.scheduler_flusher";
-  static final long FLUSHING_PERIOD_IN_MILLIS = 180 * 1000;
+  private static final String KEY_META_DATA_WAKE_UP = "com.mapbox.AdjustWakeUp";
+  static long FLUSHING_PERIOD_IN_MILLIS = 180 * 1000;
   private final Context context;
   private final AlarmReceiver alarmReceiver;
 
   SchedulerFlusherFactory(Context context, AlarmReceiver alarmReceiver) {
     this.context = context;
     this.alarmReceiver = alarmReceiver;
+    checkUpdatePeriod();
   }
 
   SchedulerFlusher supply() {
@@ -24,5 +29,21 @@ class SchedulerFlusherFactory {
     int requestCode = (int) System.currentTimeMillis();
     return new AlarmSchedulerFlusher(context, alarmManager, alarmReceiver, requestCode);
     // }
+  }
+
+  private void checkUpdatePeriod() {
+    try {
+      ApplicationInfo appInformation = context.getPackageManager().getApplicationInfo(context.getPackageName(),
+        PackageManager.GET_META_DATA);
+      if (appInformation != null && appInformation.metaData != null) {
+        boolean adjustWakeUp = appInformation.metaData.getBoolean(KEY_META_DATA_WAKE_UP, false);
+        Log.e("test", "adjustWakeUp: " + adjustWakeUp);
+        if (adjustWakeUp) {
+          FLUSHING_PERIOD_IN_MILLIS = 600000;
+        }
+      }
+    } catch (PackageManager.NameNotFoundException exception) {
+      exception.printStackTrace();
+    }
   }
 }
