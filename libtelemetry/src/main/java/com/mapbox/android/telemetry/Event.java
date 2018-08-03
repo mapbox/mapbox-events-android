82afd52770d599ff3beb53529c2f3512f247130e
Antonio Zugaldia <antonio@mapbox.com>
2018-08-03 16:23:34 -0400

    add a new mapofflineevent to the repo

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/Event.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/Event.java
@@ -8,11 +8,13 @@ import java.util.EnumSet;
 public abstract class Event implements Parcelable {
 
   public enum Type {
-    TURNSTILE, MAP_LOAD, MAP_CLICK, MAP_DRAGEND, LOCATION, NAV_DEPART, NAV_ARRIVE, NAV_CANCEL, NAV_REROUTE,
+
+    TURNSTILE, MAP_LOAD, MAP_CLICK, MAP_DRAGEND, MAP_OFFLINE, LOCATION,
+    NAV_DEPART, NAV_ARRIVE, NAV_CANCEL, NAV_REROUTE,
     NAV_FEEDBACK, NAV_FASTER_ROUTE, VIS_GENERAL, VIS_ATTACHMENT
   }
 
-  static EnumSet<Type> mapGestureEventTypes = EnumSet.of(Type.MAP_CLICK, Type.MAP_DRAGEND);
+  static EnumSet<Type> mapGestureEventTypes = EnumSet.of(Type.MAP_CLICK, Type.MAP_DRAGEND, Type.MAP_OFFLINE);
   static EnumSet<Type> navigationEventTypes = EnumSet.of(Type.NAV_DEPART, Type.NAV_ARRIVE, Type.NAV_CANCEL,
     Type.NAV_REROUTE, Type.NAV_FEEDBACK, Type.NAV_FASTER_ROUTE);
   static EnumSet<Type> visionEventTypes = EnumSet.of(Type.VIS_GENERAL, Type.VIS_ATTACHMENT);
---
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/MapOfflineEvent.java
@@ -0,0 +1,101 @@
+package com.mapbox.android.telemetry;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MapOfflineEvent extends Event implements Parcelable {
+  private static final String MAP_OFFLINE = "map.offline";
+
+  @SerializedName("event")
+  private final String event;
+  @SerializedName("created")
+  private String created;
+  @SerializedName("lat_north")
+  private double latitudeNorth;
+  @SerializedName("lat_south")
+  private double latitudeSouth;
+  @SerializedName("lon_east")
+  private double longitudeEast;
+  @SerializedName("lon_west")
+  private double longitudeWest;
+
+  MapOfflineEvent() {
+    this.event = MAP_OFFLINE;
+    this.created = TelemetryUtils.obtainCurrentDate();
+  }
+
+  private MapOfflineEvent(Parcel in) {
+    event = in.readString();
+    created = in.readString();
+    latitudeNorth = in.readDouble();
+    longitudeEast = in.readDouble();
+    latitudeSouth = in.readDouble();
+    longitudeWest = in.readDouble();
+  }
+
+  public double getLatitudeNorth() {
+    return latitudeNorth;
+  }
+
+  public void setLatitudeNorth(double latitudeNorth) {
+    this.latitudeNorth = latitudeNorth;
+  }
+
+  public double getLatitudeSouth() {
+    return latitudeSouth;
+  }
+
+  public void setLatitudeSouth(double latitudeSouth) {
+    this.latitudeSouth = latitudeSouth;
+  }
+
+  public double getLongitudeEast() {
+    return longitudeEast;
+  }
+
+  public void setLongitudeEast(double longitudeEast) {
+    this.longitudeEast = longitudeEast;
+  }
+
+  public double getLongitudeWest() {
+    return longitudeWest;
+  }
+
+  public void setLongitudeWest(double longitudeWest) {
+    this.longitudeWest = longitudeWest;
+  }
+
+  @Override
+  Type obtainType() {
+    return Type.MAP_OFFLINE;
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(event);
+    dest.writeString(created);
+    dest.writeDouble(latitudeNorth);
+    dest.writeDouble(longitudeEast);
+    dest.writeDouble(latitudeSouth);
+    dest.writeDouble(longitudeWest);
+  }
+
+  public static final Creator<MapOfflineEvent> CREATOR = new Creator<MapOfflineEvent>() {
+    @Override
+    public MapOfflineEvent createFromParcel(Parcel in) {
+      return new MapOfflineEvent(in);
+    }
+
+    @Override
+    public MapOfflineEvent[] newArray(int size) {
+      return new MapOfflineEvent[size];
+    }
+  };
+}
