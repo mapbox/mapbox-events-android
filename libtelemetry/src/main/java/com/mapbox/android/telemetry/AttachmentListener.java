11730c819c92534ab031ec9485a630f50a5b09cc
Anthony Agby <agbyaj@gmail.com>
2018-09-11 15:00:08 -0400

    Typo Fix (#212)
    - change name of varialbe from `fieldIds` to `fileIds`

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/AttachmentListener.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/AttachmentListener.java
@@ -3,7 +3,7 @@ package com.mapbox.android.telemetry;
 import java.util.List;
 
 public interface AttachmentListener {
-  void onAttachmentResponse(String message, int code, List<String> fieldIds);
+  void onAttachmentResponse(String message, int code, List<String> fileIds);
 
-  void onAttachmentFailure(String message, List<String> fieldIds);
+  void onAttachmentFailure(String message, List<String> fileIds);
 }
