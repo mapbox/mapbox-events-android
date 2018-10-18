d28bbc3c3003f6120155cdf4880691685b334e46
Andrey Li <andrey.li@mapbox.com>
2018-10-18 13:57:11 -0700

    Add support for Vision object detection event (v2)

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/Event.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/Event.java
@@ -13,13 +13,13 @@ public abstract class Event implements Parcelable {
     OFFLINE_DOWNLOAD_START, OFFLINE_DOWNLOAD_COMPLETE,
     LOCATION,
     NAV_DEPART, NAV_ARRIVE, NAV_CANCEL, NAV_REROUTE, NAV_FEEDBACK, NAV_FASTER_ROUTE,
-    VIS_GENERAL, VIS_ATTACHMENT
+    VIS_GENERAL, VIS_ATTACHMENT, VIS_OBJ_DETECTION
   }
 
   static EnumSet<Type> mapGestureEventTypes = EnumSet.of(Type.MAP_CLICK, Type.MAP_DRAGEND);
   static EnumSet<Type> navigationEventTypes = EnumSet.of(Type.NAV_DEPART, Type.NAV_ARRIVE, Type.NAV_CANCEL,
     Type.NAV_REROUTE, Type.NAV_FEEDBACK, Type.NAV_FASTER_ROUTE);
-  static EnumSet<Type> visionEventTypes = EnumSet.of(Type.VIS_GENERAL, Type.VIS_ATTACHMENT);
+  static EnumSet<Type> visionEventTypes = EnumSet.of(Type.VIS_GENERAL, Type.VIS_ATTACHMENT, Type.VIS_OBJ_DETECTION);
 
   abstract Type obtainType();
 }
