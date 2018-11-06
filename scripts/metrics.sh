41d2074484ec75b68bf21402af728c16edf9aa25
Anthony Agby <agbyaj@gmail.com>
2018-11-06 10:48:13 -0800

    add in `scripts` for server run
    - scripts added for circleci to run stuff

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -4,10 +4,10 @@ set -e
 set -o pipefail
 
 # Track individual architectures
-./check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/armeabi-v7a/libmapbox-gl.so" "Android arm-v7"
-./check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/arm64-v8a/libmapbox-gl.so"   "Android arm-v8"
-./check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/x86/libmapbox-gl.so"         "Android x86"
-./check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/x86_64/libmapbox-gl.so"      "Android x86_64"
+#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/armeabi-v7a/libmapbox-gl.so" "Android arm-v7"
+#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/arm64-v8a/libmapbox-gl.so"   "Android arm-v8"
+#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/x86/libmapbox-gl.so"         "Android x86"
+#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/x86_64/libmapbox-gl.so"      "Android x86_64"
 
 # Track overall library size
-./check_binary_size.js "libtelemetry/build/outputs/aar/libtelemetry-release.aar" "Android AAR"
+scripts/check_binary_size.js "libtelemetry/build/outputs/aar/libtelemetry-release.aar" "Android AAR"
