ee6dc91866d554253c024fc7410a74b2b0730957
Anthony Agby <agbyaj@gmail.com>
2018-11-07 13:25:32 -0800

    Remove unneeded code + rename AAR for size checker
    - remove unneeded steps within release job
    - remove unneeded items from package.json
    - remove commented out code
    - change name from Android AAR to Telemetry AAR

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -3,11 +3,5 @@
 set -e
 set -o pipefail
 
-# Track individual architectures
-#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/armeabi-v7a/libmapbox-gl.so" "Android arm-v7"
-#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/arm64-v8a/libmapbox-gl.so"   "Android arm-v8"
-#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/x86/libmapbox-gl.so"         "Android x86"
-#scripts/check_binary_size.js "libtelemetry/build/intermediates/intermediate-jars/release/jni/x86_64/libmapbox-gl.so"      "Android x86_64"
-
 # Track overall library size
-scripts/check_binary_size.js "libtelemetry/build/outputs/aar/libtelemetry-release.aar" "Android AAR"
+scripts/check_binary_size.js "libtelemetry/build/outputs/aar/libtelemetry-release.aar" "Telemetry AAR"
