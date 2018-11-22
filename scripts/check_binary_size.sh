2d57618c836b5768cfeb72b2629615873d65b9e3
Andrey Li <andrey.li@mapbox.com>
2018-11-21 20:39:24 -0800

    Publish metrics to AWS on master and release only
    Publish binary size on master and release branch only.

--- scripts/check_binary_size.sh
+++ scripts/check_binary_size.sh
@@ -5,24 +5,27 @@ set -o pipefail
 
 file_path="libtelemetry/build/outputs/aar/libtelemetry-release.aar"
 file_size=$(wc -c <"$file_path" | sed -e 's/^[[:space:]]*//')
-date=`date '+%Y-%m-%d'`
-utc_iso_date=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 label="Telemetry AAR"
-source="mobile.binarysize"
-scripts_path="scripts"
-json_name="$scripts_path/android-binarysize.json"
-json_gz="$scripts_path/android-binarysize.json.gz"
 
 # Publish to github
 "$scripts_path"/publish_to_sizechecker.js "$file_size" "$label"
 
-# Write binary size to json file
-cat >"$json_name" <<EOL
-{"sdk": "telemetry", "platform": "android", "size": ${file_size}, "created_at": "${utc_iso_date}"}
-EOL
+if [[ $CIRCLE_BRANCH == master ]] || [[ $CIRCLE_BRANCH == release-* ]]; then
+  date=`date '+%Y-%m-%d'`
+  utc_iso_date=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
+  source="mobile.binarysize"
+  scripts_path="scripts"
+  json_name="$scripts_path/android-binarysize.json"
+  json_gz="$scripts_path/android-binarysize.json.gz"
 
-# Compress json file
-gzip -f "$json_name" > "$json_gz" 
+  # Write binary size to json file
+  cat >"$json_name" <<EOL
+  {"sdk": "telemetry", "platform": "android", "size": ${file_size}, "created_at": "${utc_iso_date}"}
+  EOL
 
-# Publish to aws
-"$scripts_path"/publish_to_aws.sh $source $date $json_gz
+  # Compress json file
+  gzip -f "$json_name" > "$json_gz" 
+
+  # Publish to aws
+  "$scripts_path"/publish_to_aws.sh $source $date $json_gz
+fi
