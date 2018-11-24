232fd3e91089bee7140177c5221114cef83bbfbf
Andrey Li <andrey.li@mapbox.com>
2018-11-23 16:01:14 -0800

    Publish metrics to AWS on master and release only (#293)
    Publish metrics to AWS on master and release only

---
+++ scripts/capture_binary_size_for_aws.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+file_path="$1"
+file_size=$(wc -c <"$file_path" | sed -e 's/^[[:space:]]*//')
+scripts_path="scripts"
+date=`date '+%Y-%m-%d'`
+utc_iso_date=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
+source="mobile.binarysize"
+json_name="$scripts_path/android-binarysize.json"
+json_gz="$scripts_path/android-binarysize.json.gz"
+
+# Write binary size to json file
+cat >"$json_name" <<EOL
+{"sdk": "telemetry", "platform": "android", "size": ${file_size}, "created_at": "${utc_iso_date}"}
+EOL
+
+# Compress json file
+gzip -f "$json_name" > "$json_gz" 
+
+# Publish to aws
+"$scripts_path"/publish_to_aws.sh $source $date $json_gz
