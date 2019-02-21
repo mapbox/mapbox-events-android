b1e551cb29975b36bbf9a7b34ef97b62c2873d8c
Anthony Agby <agbyaj@gmail.com>
2019-02-21 15:14:58 -0500

    Uploader Interface
    - uploader interface and necessary changes

--- libupload/src/main/java/com/mapbox/libupload/MapboxUploader.java
+++ libupload/src/main/java/com/mapbox/libupload/MapboxUploader.java
@@ -2,8 +2,27 @@ package com.mapbox.libupload;
 
 import android.content.Context;
 
-public class MapboxUploader {
-  MapboxUploader(Context context) {
+public interface MapboxUploader <T, E> {
 
+  public void send(T data);
+
+  public void setConfiguration(Configuration configuration);
+
+  public void addListener(Listener<E> listener);
+
+  public void removeListener(Listener<E> listener);
+
+  interface Listener<F> {
+    void onSuccess(F data);
+    void onFailure(Exception exception);
+  }
+
+  interface Configuration {
+    void setUploadInterval(long interval);
+    long getUploadInterval();
+  }
+
+  interface MapboxUploadClient <B, D> {
+    public void upload(B data, D callback);
   }
 }
