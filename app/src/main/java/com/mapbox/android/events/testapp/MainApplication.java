6da9c92a13876b90a51bad9eb687c900ffe12ae4
Andrey Li <andrey.li@mapbox.com>
2019-03-07 16:13:25 -0800

    Add leak canary to the test app

--- app/src/main/java/com/mapbox/android/events/testapp/MainApplication.java
+++ app/src/main/java/com/mapbox/android/events/testapp/MainApplication.java
@@ -2,12 +2,18 @@ package com.mapbox.android.events.testapp;
 
 import android.app.Application;
 import android.os.StrictMode;
+import com.squareup.leakcanary.LeakCanary;
 
 public class MainApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      return;
+    }
+    LeakCanary.install(this);
     if (BuildConfig.DEBUG) {
       StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
         .detectDiskReads()
