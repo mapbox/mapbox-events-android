7ec687aae336fa17d53ec3c08c54998db79b8caf
Anthony Agby <agbyaj@gmail.com>
2019-03-11 12:51:14 -0400

    Add MapboxUplaoder
    - add MapboxUploader interface

---
+++ libtelemetry/src/full/java/com/mapbox/android/telemetry/MapboxUploader.java
@@ -0,0 +1,28 @@
+package com.mapbox.android.telemetry;
+
+public interface MapboxUploader<T, E> {
+
+  void send(T data);
+
+  void setConfiguration(Configuration configuration);
+
+  void addListener(Listener<E> listener);
+
+  void removeListener(Listener<E> listener);
+
+  interface Listener<F> {
+    void onSuccess(F data);
+
+    void onFailure(Exception exception);
+  }
+
+  interface Configuration {
+    void setUploadInterval(long interval);
+
+    long getUploadInterval();
+  }
+
+  interface MapboxUploadClient<B, D> {
+    void upload(B data, D callback);
+  }
+}
