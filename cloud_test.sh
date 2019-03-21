4790fd618d64d9d00aab9d08f2d7707864415239
Andrey Li <andrey.li@mapbox.com>
2019-03-20 20:35:57 -0700

    Final cleanup

--- cloud_test.sh
+++ cloud_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 set -xe
-module_name="$1"
-build_dir_outputs="$2"
-test_apk_path="$3"
+
+build_dir_outputs="$1"
+test_apk_path="$2"
 gcloud firebase test android models list
 gcloud firebase test android run --type instrumentation \
     --app app/build/outputs/apk/full/debug/app-full-debug.apk \
@@ -11,9 +11,9 @@ gcloud firebase test android run --type instrumentation \
     --device model=athene,version=23,locale=en,orientation=landscape \
     --device model=sailfish,version=26,locale=en,orientation=portrait \
     --device model=sailfish,version=28,locale=en,orientation=portrait \
-    --environment-variables coverage=true,coverageFile="/sdcard/${module_name}_coverage.ec" \
+    --environment-variables coverage=true,coverageFile="/sdcard/coverage.ec" \
     --directories-to-pull /sdcard \
     --timeout 20m
 
-covfile_path="gs://test-lab-r47d1tyt8h0hm-iku3c1i8kjrux/sailfish-28-en-portrait/artifacts/${module_name}_coverage.ec"
-gsutil cp $covfile_path "${build_dir_outputs}/code_coverage"
+covfile_path=`gsutil ls gs://test-lab-r47d1tyt8h0hm-iku3c1i8kjrux | tail -1`
+gsutil cp "$covfile_path/sailfish-28-en-portrait/artifacts/coverage.ec" "${build_dir_outputs}/code_coverage"
