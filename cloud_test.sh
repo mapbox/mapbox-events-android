939d961147d77047469786cca6465974d2338eb4
Andrey Li <andrey.li@mapbox.com>
2019-03-21 12:23:39 -0700

    Fix sdcard path

--- cloud_test.sh
+++ cloud_test.sh
@@ -2,7 +2,7 @@
 set -xe
 
 build_dir_outputs="$1"
-base_dir=$(echo "$build_dir_outputs" | cut -d "/" -f2)
+base_dir=$(echo "$build_dir_outputs" | cut -d "/" -f1)
 test_apk_path="$2"
 results_dir="$3"
 gcloud firebase test android models list
@@ -15,7 +15,7 @@ gcloud firebase test android run --type instrumentation \
     --device model=sailfish,version=26,locale=en,orientation=portrait \
     --device model=sailfish,version=28,locale=en,orientation=portrait \
     --environment-variables coverage=true,coverageFile="/sdcard/${base_dir}/coverage.ec" \
-    --directories-to-pull "sdcard/${base_dir}" \
+    --directories-to-pull /sdcard \
     --timeout 20m
 
 bucket="test-lab-r47d1tyt8h0hm-iku3c1i8kjrux"
