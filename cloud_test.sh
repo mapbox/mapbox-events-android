bedd16d179da6013272784f7ea120ca67310a6be
Andrey Li <andrey.li@mapbox.com>
2019-03-20 19:59:38 -0700

    Copy code coverage file to the local build folder

--- cloud_test.sh
+++ cloud_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 set -xe
-
-test_path="$1"
-module_name="$2"
+module_name="$1"
+build_dir_outputs="$2"
+test_apk_path="$3"
 gcloud firebase test android models list
 gcloud firebase test android run --type instrumentation \
     --app app/build/outputs/apk/full/debug/app-full-debug.apk \
-    --test "$test_path" \
+    --test "${build_dir_outputs}/{test_apk_path}" \
     --device model=hammerhead,version=21,locale=en,orientation=portrait  \
     --device model=athene,version=23,locale=en,orientation=landscape \
     --device model=sailfish,version=26,locale=en,orientation=portrait \
@@ -14,3 +14,6 @@ gcloud firebase test android run --type instrumentation \
     --environment-variables coverage=true,coverageFile="/sdcard/${module_name}_coverage.ec" \
     --directories-to-pull /sdcard \
     --timeout 20m
+
+covfile_path="gs://test-lab-r47d1tyt8h0hm-iku3c1i8kjrux/sailfish-28-en-portrait/artifacts/${module_name}_coverage.ec"
+gsutil cp covfile_path "${build_dir_outputs}/code_coverage"
