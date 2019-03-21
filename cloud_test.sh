497de13770b5724c8acc70cb4271d9f7b9d88cb2
Andrey Li <andrey.li@mapbox.com>
2019-03-20 18:14:04 -0700

    Aggregate test coverage on CI

--- cloud_test.sh
+++ cloud_test.sh
@@ -2,16 +2,15 @@
 set -xe
 
 test_path="$1"
-
+module_name="$2"
 gcloud firebase test android models list
 gcloud firebase test android run --type instrumentation \
     --app app/build/outputs/apk/full/debug/app-full-debug.apk \
     --test "$test_path" \
+    --device model=hammerhead,version=21,locale=en,orientation=portrait  \
+    --device model=athene,version=23,locale=en,orientation=landscape \
+    --device model=sailfish,version=26,locale=en,orientation=portrait \
     --device model=sailfish,version=28,locale=en,orientation=portrait \
-    --environment-variables coverage=true,coverageFile="/sdcard/coverage.ec" \
+    --environment-variables coverage=true,coverageFile="/sdcard/${module_name}_coverage.ec" \
     --directories-to-pull /sdcard \
     --timeout 20m
-
-bucket=`gsutil ls gs://test-lab-r47d1tyt8h0hm-iku3c1i8kjrux | tail -1`
-coverageFile=`gsutil ls ${bucket}sailfish-28-en-portrait/artifacts/coverage.ec`
-gsutil cp $coverageFile libcore/build
