5e9a36bc8575e4c0a2044228ebc9f995e5bf4ef7
Kevin Li <kevin.li@mapbox.com>
2019-03-22 06:34:41 +0800

    Aggregate instrumentation and unit test coverage (#343)

--- cloud_test.sh
+++ cloud_test.sh
@@ -1,14 +1,24 @@
 #!/bin/bash 
 set -xe
 
-test_path="$1"
-
+build_dir="$1"
+module=$(echo "$build_dir" | cut -d "/" -f1)
+test_apk_path="$2"
+results_dir="$3"
 gcloud firebase test android models list
 gcloud firebase test android run --type instrumentation \
     --app app/build/outputs/apk/full/debug/app-full-debug.apk \
-    --test "$test_path" \
+    --test "${build_dir}/${test_apk_path}" \
+    --results-dir="$results_dir" \
     --device model=hammerhead,version=21,locale=en,orientation=portrait  \
-    --device model=athene,version=23,locale=en,orientation=landscape \
+    --device model=hammerhead,version=23,locale=en,orientation=landscape \
     --device model=sailfish,version=26,locale=en,orientation=portrait \
     --device model=sailfish,version=28,locale=en,orientation=portrait \
+    --environment-variables coverage=true,coverageFile="/sdcard/${module}_coverage.ec" \
+    --directories-to-pull /sdcard \
     --timeout 20m
+
+bucket="test-lab-r47d1tyt8h0hm-iku3c1i8kjrux"
+artifacts_path="sailfish-28-en-portrait/artifacts"
+covfile_path="gs://${bucket}/${results_dir}/${artifacts_path}/${module}_coverage.ec"
+gsutil cp $covfile_path "${build_dir}/jacoco"
---
+++ libcore/src/test/java/com/mapbox/android/core/DummyTest.java
@@ -0,0 +1,9 @@
+package com.mapbox.android.core;
+
+import org.junit.Test;
+
+public class DummyTest {
+  @Test
+  public void dummyTestMethod() {
+  }
+}
