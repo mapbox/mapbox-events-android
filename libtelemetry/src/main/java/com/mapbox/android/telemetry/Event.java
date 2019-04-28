5b5ca7e04cb8fab31a8dc040b6e7521f930a58a4
Kevin Li <kevin.li@mapbox.com>
2019-04-28 11:07:58 +0800

    Remove nav event creation  related codes (#383)
    * Remove nav related codes
    
    * Remove configureGsonBuilder
    
    * rm audio related classes.

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/Event.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/Event.java
@@ -17,9 +17,6 @@ public abstract class Event implements Parcelable {
     VIS_GENERAL, VIS_ATTACHMENT, VIS_OBJ_DETECTION, NO_OP, CRASH
   }
 
-  static EnumSet<Type> mapGestureEventTypes = EnumSet.of(Type.MAP_CLICK, Type.MAP_DRAGEND);
-  static EnumSet<Type> navigationEventTypes = EnumSet.of(Type.NAV_DEPART, Type.NAV_ARRIVE, Type.NAV_CANCEL,
-    Type.NAV_REROUTE, Type.NAV_FEEDBACK, Type.NAV_FASTER_ROUTE);
   static EnumSet<Type> visionEventTypes = EnumSet.of(Type.VIS_GENERAL, Type.VIS_ATTACHMENT, Type.VIS_OBJ_DETECTION);
 
   Type obtainType() {
--- libtelemetry/src/main/java/com/mapbox/android/telemetry/NavBuildEvent.java
+++
@@ -1,6 +0,0 @@
-package com.mapbox.android.telemetry;
-
-interface NavBuildEvent {
-
-  Event build(NavigationState navigationState);
-}
