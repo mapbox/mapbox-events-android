99441e2fcc7cb91643f65b6c6ceb1357019972bd
Kevin Li <kevin.li@mapbox.com>
2019-12-19 03:21:52 +0800

    Add test device for CI in release process (#417)

--- cloud_test.sh
+++ cloud_test.sh
@@ -5,15 +5,44 @@ build_dir="$1"
 module=$(echo "$build_dir" | cut -d "/" -f1)
 test_apk_path="$2"
 results_dir="$3"
+is_release="$4"
+
+devices="--device model=m0,version=18,locale=en,orientation=portrait \
+--device model=hammerhead,version=21,locale=en,orientation=portrait  \
+--device model=hammerhead,version=23,locale=en,orientation=landscape \
+--device model=sailfish,version=26,locale=en,orientation=portrait \
+--device model=sailfish,version=28,locale=en,orientation=portrait \
+"
+if [ "$is_release" == "true" ]; then
+echo "Is release, adding more devices"
+devices="$devices
+--device model=g3,version=19,locale=en,orientation=portrait \
+--device model=Nexus6,version=21,locale=en,orientation=portrait \
+--device model=Nexus6,version=22,locale=en,orientation=portrait \
+--device model=Nexus6,version=23,locale=en,orientation=portrait \
+--device model=j1acevelte,version=22,locale=en,orientation=portrait \
+--device model=sailfish,version=25,locale=en,orientation=portrait \
+--device model=sailfish,version=27,locale=en,orientation=portrait \
+--device model=starqlteue,version=26,locale=en,orientation=portrait \
+--device model=taimen,version=26,locale=en,orientation=portrait \
+--device model=taimen,version=27,locale=en,orientation=portrait \
+--device model=walleye,version=26,locale=en,orientation=portrait \
+--device model=walleye,version=27,locale=en,orientation=portrait \
+--device model=walleye,version=28,locale=en,orientation=portrait \
+--device model=zeroflte,version=23,locale=en,orientation=portrait \
+--device model=hero2lte,version=23,locale=en,orientation=portrait \
+--device model=cheryl,version=25,locale=en,orientation=portrait \
+--device model=HWMHA,version=24,locale=en,orientation=portrait \
+--device model=FRT,version=27,locale=en,orientation=portrait \
+"
+fi
+
 gcloud firebase test android models list
 gcloud firebase test android run --type instrumentation \
     --app app/build/outputs/apk/full/debug/app-full-debug.apk \
     --test "${build_dir}/${test_apk_path}" \
     --results-dir="$results_dir" \
-    --device model=hammerhead,version=21,locale=en,orientation=portrait  \
-    --device model=hammerhead,version=23,locale=en,orientation=landscape \
-    --device model=sailfish,version=26,locale=en,orientation=portrait \
-    --device model=sailfish,version=28,locale=en,orientation=portrait \
+    $(echo $devices) \
     --environment-variables coverage=true,coverageFile="/sdcard/${module}_coverage.ec" \
     --directories-to-pull /sdcard \
     --timeout 20m
