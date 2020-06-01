f717b1384f283f7406f34ff75c8e09fc7def7ac1
Neeraja Kukday <neeraja.kukday@mapbox.com>
2020-06-01 10:19:31 -0700

    Endpoint configuration (#477)

---
+++ libtelemetry/src/androidTest/java/com/mapbox/android/telemetry/ServerInformationInstrumentationTest.java
@@ -0,0 +1,44 @@
+package com.mapbox.android.telemetry;
+
+import android.os.Bundle;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class ServerInformationInstrumentationTest {
+
+  @Test
+  public void comServerValidInfoTest() {
+    List<String> configurationList = new ArrayList<String>() {
+      {
+        add("qMkoTOaKYcVA8pWPtVKB7Kg46M5eccBMoa9WaEQVedQ=");
+      }
+    };
+    ComServerInformation comServerInformation = new ComServerInformation();
+    String anyAppInfoHostname = "some.test.url.com";
+    Bundle bundle = new Bundle();
+    bundle.putString("com.mapbox.ComEventsServer", anyAppInfoHostname);
+    comServerInformation.setConfigurationList(configurationList);
+
+    ServerInformation serverInformation = comServerInformation.obtainServerInformation(bundle);
+
+    assertEquals(anyAppInfoHostname, serverInformation.getHostname());
+  }
+
+  @Test
+  public void comServerInvalidInfoTest() {
+    ComServerInformation comServerInformation = new ComServerInformation();
+    String anyAppInfoHostname = "some.test.url.com";
+    Bundle bundle = new Bundle();
+    bundle.putString("com.mapbox.ComEventsServer", anyAppInfoHostname);
+
+    ServerInformation serverInformation = comServerInformation.obtainServerInformation(bundle);
+
+    assertEquals(serverInformation.getHostname(), null);
+  }
+}
--- libtelemetry/src/main/java/com/mapbox/android/telemetry/ComServerInformation.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/ComServerInformation.java
@@ -2,8 +2,24 @@ package com.mapbox.android.telemetry;
 
 
 import android.os.Bundle;
+import android.support.annotation.VisibleForTesting;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
 
 class ComServerInformation implements EnvironmentResolver {
+  private final String LOG_TAG = "ComServerInformation";
+  private final String DIGEST = "SHA-256";
+  private final String KEY_META_DATA_COM_SERVER = "com.mapbox.ComEventsServer";
+  private List<String> configurationList =
+    new ArrayList<String>() {
+      {
+        add("FVQ3CP/SEI8eLPxHJnjyew2P5DTC1OBKK4Y6XkmC0WI=");
+      }
+    };
 
   @Override
   public void nextChain(EnvironmentResolver chain) {
@@ -12,6 +28,32 @@ class ComServerInformation implements EnvironmentResolver {
   @Override
   public ServerInformation obtainServerInformation(Bundle appMetaData) {
     ServerInformation com = new ServerInformation(Environment.COM);
+    String hostname = appMetaData.getString(KEY_META_DATA_COM_SERVER);
+    if (!TelemetryUtils.isEmpty(hostname)) {
+      String hostnameHash = obtainHash(hostname);
+      if (!TelemetryUtils.isEmpty(hostnameHash)
+        && configurationList.contains(hostnameHash)) {
+        com.setHostname(hostname);
+      }
+    }
     return com;
   }
+
+  private String obtainHash(String hostname) {
+    String hostNameHash = null;
+    try {
+      byte[] digest = MessageDigest.getInstance(DIGEST).digest(hostname.getBytes());
+      hostNameHash = Base64.encodeToString(digest, Base64.NO_WRAP);
+    } catch (Exception exception) {
+      Log.d(LOG_TAG, String.format("Hostname error %s", exception.getMessage()));
+    }
+
+    return hostNameHash;
+  }
+
+  @VisibleForTesting
+  void setConfigurationList(List<String> configurationList) {
+    this.configurationList = configurationList;
+  }
+
 }
