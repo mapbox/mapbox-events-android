da0af44ff0946089a92488c8c0821838052baf69
bhavika <bhavicka.7992@gmail.com>
2021-05-05 10:38:16 -0400

    Add new staging pins

--- libtelemetry/src/main/java/com/mapbox/android/telemetry/StagingCertificatePins.java
+++ libtelemetry/src/main/java/com/mapbox/android/telemetry/StagingCertificatePins.java
@@ -16,6 +16,12 @@ class StagingCertificatePins {
           add("5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w=");
           add("r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=");
           add("PA1lecwXNRXY/Vpy0VN+jQEYChN4hCAF36oB0Ygx3wQ=");
+          // new 2048 bit keys generated in April 2021
+          add("8apXPecP7X3vUGqi/B42cig4O1BjQUM4dng5gMVOiK0=");
+          add("MxGjtNVZ0mEdjhhfvAcTNZd+lC8WY8vKkkaSFE2azXQ=");
+          add("i/5fi5jB13JKeiZJMFNu4XSIaaCNmxAWsWvmMsI7t5s=");
+          add("4YJLMcE66WP2/FRID2HT0QpQRNjG7zqz/dJzP3BGct8=");
+          add("H1YTKuZacKUYyGnQFVPcarkqYxvGJ7QKb9dFz2TssKw=");
         }
       });
     }
